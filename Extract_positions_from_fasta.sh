50cf4cdeba28bf6589c4a44c3c60e6fc237b5d6e
nnneeennn <nina_hollfelder@hotmail.com>
2022-03-17 10:28:41 +0100

    Update Extract_positions_from_fasta.sh

--- Extract_positions_from_fasta.sh
+++ Extract_positions_from_fasta.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #EXTRACTS SINGLE POSITIONS (DEFINED IN PLINK .BIM FILE) FROM REFERENCE FASTA SEQUENCE (OR ANY FASTA SEQUENCE)
-#NEED TO DEFINE $ref REFERENCE FASTA 
+#should be run as command bimfile reference
 
 cat $1.bim | while read line  
 do 
 chr=$(echo $line | cut -d" "  -f1 ); pos=$(echo $line | cut -d" "  -f4 )
-allele=$(samtools faidx $ref ${chr}:${pos}-${pos} | tail -n1)
-if [ -z $allele ]; then 
+allele=$(samtools faidx $2 ${chr}:${pos}-${pos} | tail -n1)
+if [ ! -z $allele ]; then 
 echo "$allele" >>refallele
 else echo "empty" >>refallele
 fi
